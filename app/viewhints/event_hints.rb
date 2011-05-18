410518a7a8de04c82a369615e97a36ac6b4a72ec
Umur Ozkul <umur.ozkul@gmail.com>
2011-05-18 14:59:11 +0200

    Cours Areas are child of Event

--- app/viewhints/event_hints.rb
+++ app/viewhints/event_hints.rb
@@ -5,6 +5,6 @@ class EventHints < Hobo::ViewHints
   # field_help :field1 => "Enter what you want in this field"
   # children :primary_collection1, :aside_collection1, :aside_collection2
 
-  children :enrollments, :races, :calendar_entries
+  children :enrollments, :races, :calendar_entries, :course_areas
 
 end
