3869ac57e6ce4e9921729b2e89295315cc1d463e
Umur Ozkul <umur.ozkul@gmail.com>
2011-08-12 14:43:36 +0200

    allow nil time zone

--- app/rich_types/time_zone_rich_type.rb
+++ app/rich_types/time_zone_rich_type.rb
@@ -1,30 +1,26 @@
 class TimeZoneRichType < String
   COLUMN_TYPE = :string
   HoboFields.register_type(:time_zone, self)
-  
-  def validate
-    "is not a valid time zone" if self.to_tz.nil?
-  end
-  
+
 #    def format
 #     self.strip if self
 #    end
-  
+
    def to_html(xmldoctype = true)
     self.to_tz.to_s
    end
-  
+
   def is_number?
     self =~ /^ *[+-]*[0-9]+ *$/
   end
-  
+
   def to_tzindex
     return self.to_i if self.is_number?
     return self
   end
-  
+
   def to_tz
     ActiveSupport::TimeZone[self.to_tzindex]
   end
-  
+
 end
